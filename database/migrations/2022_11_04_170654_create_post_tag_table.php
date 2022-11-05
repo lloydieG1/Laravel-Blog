1d9c46c17cda15226ac0407ef17f9d1b376350aa
lloydieG1 <lloydieG1@gmail.com>
2022-11-05 12:36:58 +0000

    schema is now post_tag

--- database/migrations/2022_11_04_170654_create_post_tag_table.php
+++ database/migrations/2022_11_04_170654_create_post_tag_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tag_post', function (Blueprint $table) {
-            $table->primary(['tag_id', 'post_id']);
-            $table->bigInteger('tag_id')->unsigned();
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->primary(['post_id', 'tag_id']);
             $table->bigInteger('post_id')->unsigned();
+            $table->bigInteger('tag_id')->unsigned();
             $table->timestamps();
 
-            $table->foreign('tag_id')->references('id')->on('tags')
+            $table->foreign('post_id')->references('id')->on('posts')
                 ->onDelete('cascade')->onUpdate('cascade');
 
-            $table->foreign('post_id')->references('id')->on('posts')
+            $table->foreign('tag_id')->references('id')->on('tags')
                 ->onDelete('cascade')->onUpdate('cascade');
         });
     }
