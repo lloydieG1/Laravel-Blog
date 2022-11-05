d6b23a14311e9d688ffe05558255bb488ad32b4c
lloydieG1 <lloydieG1@gmail.com>
2022-11-05 13:31:10 +0000

    Comment factory generates fake data

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Page>
@@ -17,7 +18,8 @@ class PageFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'description' => fake()->paragraph(),
+            'user_id' => User::factory()
         ];
     }
 }
