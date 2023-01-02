d789e0bb57f6c0bf672e17598cdae914714168f5
lloydieG1 <lloydieG1@gmail.com>
2023-01-02 16:25:23 +0000

    names added to routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('app');
+    return view('home');
 });
 
-Route::get('/users', [UserController::class,'index']);
-Route::get('/users/{id}', [UserController::class,'show']);
+Route::get('/users', [UserController::class,'index'])->name('users.index');
+Route::get('/users/{id}', [UserController::class,'show'])->name('users.show');
