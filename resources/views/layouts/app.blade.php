74c91d73e22531aa98c3037a5cd89deade4536c9
lloydieG1 <lloydieG1@gmail.com>
2023-01-02 16:26:21 +0000

    navbar added to defult template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,13 @@
         <title>Blog - @yield('title')</title>
     </head>
     <body>
+        <nav class="navbar">
+            <a href="/">Home</a>
+            <a href="/users">Users</a>
+            <a href="/tags">Tags</a>
+            <a href="/page">My page</a>
+        </nav>
+
         <h1>Blog - @yield('title')</h1>
  
         <div class="container">
