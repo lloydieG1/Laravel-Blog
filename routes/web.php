05bf20a37b8b9e5912a9208c02adf7d1795e0866
lloydieG1 <lloydieG1@gmail.com>
2023-01-02 19:03:14 +0000

    merge

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/users', [UserController::class,'index'])->name('users.index');
 
