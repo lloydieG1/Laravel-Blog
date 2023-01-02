ab9b112430cd4c9492e330cdc59a187ff27188a5
lloydieG1 <lloydieG1@gmail.com>
2023-01-02 15:15:46 +0000

    created simple default layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog - @yield('title')</title>
+    </head>
+    <body>
+        <h1>Blog - @yield('title')</h1>
+ 
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
