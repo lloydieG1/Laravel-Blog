2f56f783d7a46fcd7aef819f8fbf04ddf51e5d62
lloydieG1 <lloydieG1@gmail.com>
2023-01-03 18:27:05 +0000

    if user is logged in, display log out and create post button

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,16 +2,21 @@
 <html>
     <head>
         <title>Blog - @yield('title')</title>
-        <link rel="stylesheet" href="resources/css/app.css">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body class="bg-blue-700">
         <nav class="content-center">
             <a href="/">Home</a>
             <a href="/users">Users</a>
             <a href="/tags">Tags</a>
-            <a href="/page">My page</a>
-            <a href="/login">Log in</a>
+            <a href="/mypage">My page</a>
             <a href="/register">Register</a>
+            @if (auth()->check())
+                <a href="/mypage/createpost">Create Post</a>
+                <a href="/logout">Log out</a>
+            @else
+                <a href="/login">Log in</a>
+            @endif
         </nav>
 
         <h1>Blog - @yield('title')</h1>
