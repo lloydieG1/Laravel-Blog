14de4ea6ec274d05d9dc63f8d60ca6b69a3f3236
lloydieG1 <lloydieG1@gmail.com>
2023-01-04 18:43:41 +0000

    fixed user index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
     <p>List of users:</p>
     <ul>
         @foreach ($users as $user)
-            <li><a href="{{ route('users.show', ['id' => $user->id]) }}"> {{ $user->name }} </a></li>
+            <li><a href="{{ route('page.show', ['id' => $user->page->id]) }}"> {{ $user->name }} </a></li>
         @endforeach
     </ul>
     {{ $users->links() }}
