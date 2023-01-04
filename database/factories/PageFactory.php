0fb4a0e436939b874f6e2195a63d270cb1a80840
lloydieG1 <lloydieG1@gmail.com>
2023-01-04 14:14:33 +0000

    fixed page title seeding

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -18,7 +18,7 @@ class PageFactory extends Factory
     public function definition()
     {
         return [
-            'title' => fake()->paragraph(),
+            'title' => fake()->word(),
             'description' => fake()->paragraph(),
             'user_id' => User::factory()
         ];
