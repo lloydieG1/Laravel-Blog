65826ff5422daa91e3b7ea738b03eae177ac102c
lloydieG1 <lloydieG1@gmail.com>
2023-01-04 11:01:20 +0000

    added CRUD commands for postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,25 +1,98 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
-use App\Models\User;
- 
-class UserController extends Controller
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
 {
-    public function show($id)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return view('users.page', [
-            'user' => User::findOrFail($id)
-        ]);
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
-    public function create(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'tag' => 'nullable|max:30'
+        ]);
+        $post = Post::create($validatedData);
+        return redirect('/posts/' . $post->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
     {
         $validatedData = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required',
         ]);
-        $post = Post::create($validatedData);
+        $post->update($validatedData);
         return redirect('/posts/' . $post->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/posts');
     }
 }
