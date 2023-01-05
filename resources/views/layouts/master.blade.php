4c47c914b263877ebf3cd9b5024e79edbef7f0c6
lloydieG1 <lloydieG1@gmail.com>
2023-01-05 19:34:33 +0000

    can now only register if not logged in

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,9 @@
     <body class="bg-slate-200 font-sans leading-normal tracking-normal">
         <nav class="bg-white py-6 shadow-md">
             <div class="container mx-auto px-6 flex items-center justify-between">
-                <a href="/"><img src="/images/logo.png" alt="Logo" class="h-20 w-240"></a>
+                <a href="/"><img src="/images/logo.png" alt="Logo" class="h-20 w-200" height="20" width="200"></a>
                 <a href="/users" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Users</a>
                 <a href="/tags" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Tags</a>
-                <a href="/register" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Register</a>
                 @auth
                     @if (is_null(Auth::user()->page))
                         <a href="{{ route('page.create') }}" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Create page</a>
@@ -23,10 +22,15 @@
                     <a href="/logout" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Log out</a>
                 @else
                     <a href="/login" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Log in</a>
+                    <a href="/register" class="mr-4 text-gray-800 hover:text-gray-600 no-underline">Register</a>
                 @endauth
             </div>
         </nav>
-        <h1>Blog - @yield('title')</h1>
+        <div class="container mx-auto px-6 pt-10 pb-8">
+            <h1 class="text-4xl font-bold text-gray-800 text-center mb-8">
+                @yield('title')
+            </h1>
+        </div>
 
         <div class="container" class="container mx-auto px-6 pt-10 pb-8">
             @yield('content')
