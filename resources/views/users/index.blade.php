c14ab779e819ed71a3e97f63d4dbef54fc174007
lloydieG1 <lloydieG1@gmail.com>
2023-01-05 19:35:31 +0000

    css for showing user index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,10 +3,16 @@
 @section('title', 'users')
 
 @section('content')
-    <p>List of users:</p>
-    <ul>
+    <ul class="list-reset">
         @foreach ($users as $user)
-            <li><a href="{{ route('page.show', ['id' => $user->page->id]) }}"> {{ $user->name }} </a></li>
+            <li class="px-4 py-2 mb-2 bg-white shadow-md rounded-md">
+                <a class="block no-underline hover:text-gray-800"
+                    href="{{ route('page.show', ['id' => $user->page->id]) }}">
+                    <div class="flex items-center">
+                        <div class="text-lg font-bold text-gray-800">{{ $user->name }}</div>
+                    </div>
+                </a>
+            </li>
         @endforeach
     </ul>
     {{ $users->links() }}
