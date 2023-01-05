eade4644809f40084f7416f0fbc2337601c07b7e
lloydieG1 <lloydieG1@gmail.com>
2023-01-05 19:35:11 +0000

    some simple css for showing a page

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -1,19 +1,26 @@
 @extends('layouts.master')
 
-@section('title', 'Page')
+@section('title', $page->title)
 
 @section('content')
-    <h2>{{ $page->title }}</h2>
-    <p>{{ $page->description }}</p>
-    <h3>Details:</h3>
-    <ul>
-       <li>Name: {{$page->user->name}}</li>
-       <li>Email: {{$page->user->email}}</li>
-    </ul>
-    <p>Posts:</p>
+    <div class="border-2 border-gray-200 rounded-md p-4">
+        <p class="text-lg font-bold text-gray-800 mb-2">Description</p>
+        <p class="text-gray-700">{{ $page->description }}</p>
+    </div>
+    <div class="flex items-center mb-8">
+        <img src="/images/logo.png" alt="Avatar" class="h-24 w-24 rounded-full mr-4">
+        <div class="text-3xl font-bold text-gray-800">{{ $page->user->name}}</div>
+    </div>
+    <h2 class="text-2xl font-bold text-gray-800 mb-8">Posts</h2>
     <ul>
         @foreach ($page->posts as $post)
-            <li>Title: {{ $post->title }} <br> {{ $post->body }}</li>
+        <li class="px-4 py-2 mb-2 bg-white shadow-md rounded-md">
+            <a href="/posts/{{ $post->id }}" class="block no-underline hover:text-gray-800">
+              <div class="flex items-center">
+                <div class="text-lg font-bold text-gray-800">{{ $post->title }}</div>
+              </div>
+            </a>
+          </li>
         @endforeach
     </ul>
 @endsection
