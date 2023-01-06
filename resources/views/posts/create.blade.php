89f3a0dcdb06b830eba3d0b07366967869a8da22
lloydieG1 <lloydieG1@gmail.com>
2023-01-06 18:59:48 +0000

    better css for create post view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,12 +5,13 @@
 @section('content')
     <form method = "POST" action="{{ route('post.store') }}" enctype="multipart/form-data">
         @csrf
-        <p>Title: <input type="text" name="title" value="{{ old('title') }}"></p>
-        <p>Body: <input type="text" name="body" value="{{ old('body') }}"></p>
-        <p>Tag: <input type="text" name="tag" value="{{ old('tag') }}"></p>
+        <p>Title: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="title" value="{{ old('title') }}"></p>
+        <p>Body: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="body" value="{{ old('body') }}"></p>
+        <p>Tag: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="tag" value="{{ old('tag') }}"></p>
         <input type="file" name="image_url" class="w-full rounded-md p-4 border-2 border-gray-200">
-        <input type="submit" value="Submit">
         <input type="hidden" name="page_id" value="{{ Auth::user()->page->id }}">
-        <a href="{{ route('page.show', ['id' => Auth::user()->page->id]) }}">Cancel</a>
+        <input type="submit" value="Submit" class="px-4 py-2 mt-8 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
+        <a href="{{ route('page.show', ['id' => Auth::user()->page->id]) }}"
+            class="px-4 py-2 mt-8 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">Cancel</a>
     </form>
 @endsection
