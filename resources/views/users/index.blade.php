ce2adef660e1a73daec94e7a5abc6e1d7fd5254d
lloydieG1 <lloydieG1@gmail.com>
2023-01-07 12:56:10 +0000

    only users that have created their page appear in the index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,14 +5,16 @@
 @section('content')
     <ul class="list-reset">
         @foreach ($users as $user)
-            <li class="px-4 py-2 mb-3 bg-gray-200 hover:bg-gray-300 shadow-md rounded-md">
-                <a class="block no-underline hover:text-gray-800"
-                    href="{{ route('page.show', ['id' => $user->page->id]) }}">
-                    <div class="flex items-center">
-                        <div class="text-lg font-bold text-gray-800">{{ $user->name }}</div>
-                    </div>
-                </a>
-            </li>
+            @if(isset($user->page))
+                <li class="px-4 py-2 mb-3 bg-gray-200 hover:bg-gray-300 shadow-md rounded-md">
+                    <a class="block no-underline hover:text-gray-800"
+                        href="{{ route('page.show', ['id' => $user->page->id]) }}">
+                        <div class="flex items-center">
+                            <div class="text-lg font-bold text-gray-800">{{ $user->name }}</div>
+                        </div>
+                    </a>
+                </li>
+            @endif
         @endforeach
     </ul>
     {{ $users->links() }}
