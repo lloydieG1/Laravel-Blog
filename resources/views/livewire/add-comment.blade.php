418ab85e26c850883c7e03b00b3a903978c6b111
lloydieG1 <lloydieG1@gmail.com>
2023-01-07 12:57:15 +0000

    fixed spacing issue

--- resources/views/livewire/add-comment.blade.php
+++ resources/views/livewire/add-comment.blade.php
@@ -4,7 +4,7 @@
     <h2 class="text-2xl font-bold text-gray-800 mb-4"><br>Comments</h2>
     @auth
         <textarea wire:model="body" rows="4" class="w-full rounded-md p-4 border-2 border-gray-200" placeholder="Leave a comment..."></textarea>
-        <button wire:click="addComment" type="submit" class="px-4 py-2 mt-4 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
+        <button wire:click="addComment" type="submit" class="mb-8 px-4 py-2 mt-4 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
             Comment
         </button>
     @else
