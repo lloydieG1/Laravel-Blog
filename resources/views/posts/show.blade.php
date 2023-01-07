0031cdf31f0c2a910ab343508c21ee06a8d6b09e
lloydieG1 <lloydieG1@gmail.com>
2023-01-07 09:36:22 +0000

    can only edit/delete posts if you are its owner OR admin

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,25 +14,29 @@
     <div class="mb-8">
         {{ $post->body }}
      </div>
-    <div>
-        @if ($post->image_url)
-            <img src="{{ asset('storage/' . $post->image_url) }}" alt="{{ $post->title }}" class="w-320 h-180 mb-4" width="320" height="180">
+     @auth
+        @if (Auth::user()->id == $post->page->user->id || Auth::user()->role == 'admin')
+            <div>
+                @if ($post->image_url)
+                    <img src="{{ asset('storage/' . $post->image_url) }}" alt="{{ $post->title }}" class="w-320 h-180 mb-4" width="320" height="180">
+                @endif
+            </div>
+            <div>
+                <a href="/posts/{{ $post->id }}/edit" class="px-4 py-2 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
+                    Edit
+                </a>
+            </div>
+            <div>
+                <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="px-4 py-2 bg-red-600 text-white float-right font-bold rounded-full shadow-md hover:bg-red-700">
+                        Delete
+                    </button>
+                </form>
+            </div>
         @endif
-    </div>
-    <div>
-        <a href="/posts/{{ $post->id }}/edit" class="px-4 py-2 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
-            Edit
-        </a>
-    </div>
-    <div>
-        <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="px-4 py-2 bg-red-600 text-white float-right font-bold rounded-full shadow-md hover:bg-red-700">
-                Delete
-            </button>
-        </form>
-    </div>
+    @endauth
     <div class="mb-8">
     <div>
         <livewire:add-comment :model='$post' />
