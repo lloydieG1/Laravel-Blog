ca8b7e09b702e04b6219b5707b901416858b1e61
lloydieG1 <lloydieG1@gmail.com>
2023-01-07 12:56:34 +0000

    css improvement for page creation

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -8,8 +8,8 @@
         <p>Name your page: <input type="text" name="title" value="{{ old('title') }}"></p>
         <p>Give your page a description: <input type="text"
             name="description" value="{{ old('description') }}"></p>
-        <input type="submit" value="Submit">
+        <input class="px-4 py-2 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900" type="submit" value="Submit">
         <input type="hidden" name="user_id" value={{Auth::user()->id}}>
-        <a href="{{ route('home') }}">Cancel</a>
+        <a class="px-4 py-2 bg-red-500 text-white font-bold rounded-full shadow-md hover:bg-gray-900" href="{{ route('home') }}">Cancel</a>
     </form>
 @endsection
