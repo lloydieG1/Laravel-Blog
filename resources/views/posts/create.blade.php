5c6b62de34d1b2fdd15aef8ecc9e9b648a5f8530
lloydieG1 <lloydieG1@gmail.com>
2023-01-07 09:35:33 +0000

    better css for post create view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,9 +5,15 @@
 @section('content')
     <form method = "POST" action="{{ route('post.store') }}" enctype="multipart/form-data">
         @csrf
-        <p>Title: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="title" value="{{ old('title') }}"></p>
-        <p>Body: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="body" value="{{ old('body') }}"></p>
-        <p>Tag: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="tag" value="{{ old('tag') }}"></p>
+        <p class="block font-bold text-2xl mb-2">
+            Title: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="title" value="{{ old('title') }}">
+        </p>
+        <p class="block font-bold text-2xl mb-2">
+            Body: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="body" value="{{ old('body') }}">
+        </p>
+        <p class="block font-bold text-2xl mb-2">
+            Tag: <input class="w-full rounded-md p-4 border-2 border-gray-200" type="text" name="tag" value="{{ old('tag') }}">
+        </p>
         <input type="file" name="image_url" class="w-full rounded-md p-4 border-2 border-gray-200">
         <input type="hidden" name="page_id" value="{{ Auth::user()->page->id }}">
         <input type="submit" value="Submit" class="px-4 py-2 mt-8 bg-gray-800 text-white font-bold rounded-full shadow-md hover:bg-gray-900">
